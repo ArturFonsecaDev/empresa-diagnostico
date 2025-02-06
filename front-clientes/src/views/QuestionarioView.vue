58ada182b1e7fff6b864d06e2341e319f806eb87
trdntb <130498186+trdntb@users.noreply.github.com>
2025-02-06 13:09:23 -0300

    .

--- front-clientes/src/views/QuestionarioView.vue
+++ front-clientes/src/views/QuestionarioView.vue
@@ -1,61 +1,147 @@
 <template>
   <div class="fundo">
     <q-card>
-      <q-form @submit.prevent="submitForm">
-        <div v-for="pergunta in perguntas" :key="pergunta.id" class="pergunta-container">
+      <!-- Perguntas -->
+      <q-form @submit.prevent="submitForm" v-if="isThereAnyPergunta && !respostasEnviadas">
+        <div v-for="pergunta in perguntas" :key="pergunta.id" class="pergunta-container" :class="{ 'erro': erros.includes(pergunta.id) }">
           <h2>{{ pergunta.texto_pergunta }}</h2>
-          <div v-for="resposta in respostasFiltradas(pergunta.id)" :key="resposta.id" class="resposta-container">
-            <button type="button" class="btn-resposta" :class="{ 'btn-active': isBtnActive}">{{ resposta.texto_resposta }}</button>
-          </div>
-            <hr>
+          <q-option-group
+            v-if="getOpcoes(pergunta.id).length"
+            v-model="respostasCliente[pergunta.id]"
+            :options="getOpcoes(pergunta.id).map(resposta => ({ 
+              label: resposta.texto_resposta, 
+              value: resposta.id 
+            }))"
+          />
+          <p v-if="erros.includes(pergunta.id)" class="mensagem-erro">Esta pergunta precisa ser respondida.</p>
+          <hr />
         </div>
         <q-btn type="submit" color="primary" label="Enviar" />
       </q-form>
+      <!-- sem perguntas -->
+      <div class="sem-pergunta" v-else-if="!respostasEnviadas">
+        <h2>Nenhuma pergunta cadastrada</h2>
+      </div>
+      <!-- respostas enviadas -->
+      <div class="respostas-enviadas" v-else>
+        <h2>O seu Resultado é:</h2>
+        <p><span class="verde">{{categoriaPredominante}}</span></p>
+      </div>
     </q-card>
   </div>
 </template>
 
 <script>
 import { getPerguntas, getRespostas } from '@/api/index.js';
+import { QCard, QForm, QOptionGroup, QBtn } from 'quasar';
+import { mapGetters } from 'vuex';
 
 export default {
+  name: 'MeuFormulario',
+  components: {
+    QCard,
+    QForm,
+    QOptionGroup,
+    QBtn,
+  },
   data() {
     return {
       perguntas: [],
       respostas: [],
       respostasCliente: [],
-      isBtnActive: false
+      respostasEnviadas: false,
+      categoriaPredominante: '',
+      erros: [] // Armazena perguntas não respondidas
     };
   },
-  computed: {
-    perguntasLength() {
-      return this.perguntas.length;
+  methods: {
+    getOpcoes(perguntaId) {
+      return this.respostas.filter(
+        (resposta) => resposta.pergunta_id === perguntaId
+      );
+    },
+    async submitForm() {
+      this.erros = [];
+      this.respostasEnviadas = false;
+
+      if (!this.getClienteAtual) {
+        console.error('Cliente não encontrado');
+        alert('Cliente não encontrado');
+        return;
+      }
+
+      // Valida se todas as perguntas foram respondidas
+      this.perguntas.forEach((pergunta) => {
+        if (!this.respostasCliente[pergunta.id]) {
+          this.erros.push(pergunta.id);
+        }
+      });
+
+      if (this.erros.length > 0) {
+        alert('Por favor, responda todas as perguntas antes de enviar.');
+        return;
+      }
+
+      let expected_body = Object.entries(this.respostasCliente)
+        .map(([key, obj]) => {
+          console.log(key);
+          return {
+          cliente_id: this.getClienteAtual.id,
+          resposta_id: obj
+          }
+        });
+
+      expected_body = {
+        respostas: expected_body
+      };
+
+      console.warn('Respostas enviadas:', expected_body);
+      const url = `http://127.0.0.1:8000/api/respostas-clientes`;
+      const data = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(expected_body),
+      });
+      const response = await data.json();
+
+      if (data.status !== 200) {
+        console.error('Erro ao enviar respostas:', response);
+        alert('Erro ao enviar respostas');
+        return;
+      }
+      console.log(response);
+
+      this.categoriaPredominante = response.descricao_categoria;
+
+      console.log('SUCESSO!!');
+      this.respostasEnviadas = true;
     },
   },
-  methods: {
-    respostasFiltradas(perguntaId) {
-      return this.respostas.filter(resposta => resposta.pergunta_id === perguntaId);
-    },
-    salvarResposta(resposta){
-      this.respostasCliente.push(resposta);
-    },
-    submitForm() {
-      console.log("Formulário enviado");
-    },
-    changeColor(){
-      this.isBtnActive =!this.isBtnActive;
-      this.$refs.btnSubmit.setDisabled(this.isBtnActive);
-      setTimeout(() => {
-        this.isBtnActive = false;
-        this.$refs.btnSubmit.setDisabled(false);
-      }, 3000);
+  computed: {
+    ...mapGetters(['getClienteAtual']),
+    isThereAnyPergunta() {
+      return this.perguntas.length > 0;
     }
   },
   async mounted() {
-    const [dataPerguntas, dataRespostas] = await Promise.all([getPerguntas(), getRespostas()]);
-    this.perguntas = dataPerguntas;
-    this.respostas = dataRespostas;
-  }
+    this.respostasCliente = [];
+    try {
+      const [dataPerguntas, dataRespostas] = await Promise.all([
+        getPerguntas(),
+        getRespostas(),
+      ]);
+      this.perguntas = dataPerguntas;
+      this.respostas = dataRespostas;
+
+      this.perguntas.forEach((pergunta) => {
+        this.$set(this.respostasCliente, pergunta.id, null);
+      });
+    } catch (error) {
+      console.error('Erro ao carregar os dados:', error);
+    }
+  },
 };
 </script>
 
@@ -65,9 +151,8 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  background-image: url('@/assets/fundo-cadastro.jpg');
+  background: url(~@/assets/fundo-cadastro.jpg) no-repeat center center;
   background-size: cover;
-  background-position: center;
   overflow: auto;
   padding: 20px;
 }
@@ -89,34 +174,56 @@ export default {
   text-align: left;
 }
 
-.resposta-container {
-  padding: 10px;
-}
-
-.btn-resposta {
-  width: 4rem;
-}
-
-h2{
+h2 {
   font-size: 1.25rem;
   line-height: 2rem;
   font-weight: bold;
 }
 
-.btn-resposta {
-  background-color: #027be3; /* Cor primária do Quasar */
-  color: white; /* Cor do texto */
-  border: none;
-  padding: 8px 16px;
-  border-radius: 4px;
-  font-size: 14px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
+/* Estiliza perguntas com erro */
+.erro {
+  border: 2px solid red;
+  padding: 10px;
+  border-radius: 5px;
 }
 
-.btn-resposta:hover, .btn-active {
-  background-color: #01279b; /* Cor mais escura no hover */
+/* Mensagem de erro */
+.mensagem-erro {
+  color: red;
+  font-size: 0.9rem;
+  margin-top: 5px;
 }
 
+.respostas-enviadas {
+  text-align: center;
+  padding: 20px;
+  background-color: rgba(0, 128, 0, 0.1); /* Fundo sutil para destacar a seção */
+  border-radius: 10px;
+  margin-top: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+.respostas-enviadas h2 {
+  font-size: 1.5rem;
+  color: #2c3e50; /* Tom escuro para melhor contraste */
+  margin-bottom: 15px;
+}
+
+.resultado-texto {
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #333;
+}
+
+.verde {
+  display: inline-block;
+  background-color: #28a745;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-weight: bold;
+  font-size: 1.2rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
 
 </style>
